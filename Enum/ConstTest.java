dcdcc3d2de9ee28eab1b8231a1d4d34d6f08b1ad
D3vL4dy <kangji1218@kakao.com>
2022-03-02 12:41:34 +0900

    Enum

---
+++ Enum/ConstTest.java
@@ -0,0 +1,13 @@
+package kr.or.ddit.basic.enumTest;
+
+public class ConstTest {
+	// static을 붙이지 않으면 EnumTest main()에서 객체를 생성해서 사용해야 한다.
+	public final static int RED = 1;
+	public final static int GREEN = 2;
+	public final static int BLUE = 3;
+	
+	public final static int ONE = 1;
+	public final static int TWO = 2;
+	public final static int THREE = 3;
+	
+}
